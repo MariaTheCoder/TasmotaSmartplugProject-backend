1ba559d5bd2047c2b455c65f91abaddb1b97f5c4
Maria T. Andersen <mteilmann93@gmail.com>
2023-07-17 15:29:47 +0200

    Distinguish between production and development by checking enviroment variable NODE_ENV. If NODE_ENV is not specified, set to the enviroment to development mode

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const serverSettings = require("./settings.json");
 const express = require("express");
+const cors = require("cors");
 // const fetch = (...args) =>
 //   import("node-fetch").then(({ default: fetch }) => fetch(...args));
 
@@ -7,38 +8,50 @@ const express = require("express");
 const app = express();
 const port = 5502;
 
-app.get("/", async (req, res) => {
-  // The code below can be used as mock data for this project
-  const devices = new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve(
-        serverSettings.devices.map((device) => require(`./${device}.json`))
-      );
-    }, 3000);
-  });
+// enable all cors requests
+app.use(cors());
 
-  devices
-    .then((value) => {
-      res.status(200).send(value);
-      console.log(value.length);
-    })
-    .catch((err) => {
-      console.error(err);
+app.get("/", async (req, res) => {
+  const environment = process.env.NODE_ENV || "development";
+
+  if (environment === "development") {
+    console.log("running in " + environment);
+    // The code below can be used as mock data for this project
+    const devices = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(
+          serverSettings.devices.map((device) => require(`./${device}.json`))
+        );
+      }, 3000);
     });
 
-  // Use this call to fetch api on Kerim's end
-  // const promises = serverSettings.devices.map((device) =>
-  //   fetch(`http://${device}/cm?cmnd=STATUS%200`)
-  //     .then((res) => res.json())
-  //     .then((data) =>
-  //       res
-  //         .status(200)
-  //         .send(data)
-  //         .catch((err) => console.error(err))
-  //     )
-  // );
-  // const results = await Promise.allSettled(promises);
-  // console.log(results.length);
+    devices
+      .then((value) => {
+        res.json(value);
+        console.log(value.length);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }
+
+  if (environment === "production") {
+    // Use this call to fetch api on Kerim's end
+    console.log("running in " + environment);
+
+    const promises = serverSettings.devices.map((device) =>
+      fetch(`http://${device}/cm?cmnd=STATUS%200`)
+        .then((res) => res.json())
+        .then((data) =>
+          res
+            .status(200)
+            .send(data)
+            .catch((err) => console.error(err))
+        )
+    );
+    const results = await Promise.allSettled(promises);
+    console.log(results.length);
+  }
 });
 
 app.listen(port, () => {
