e98752a6dcc871563469e51e70c0057dd16dd556
Maria T. Andersen <mteilmann93@gmail.com>
2023-07-17 13:10:39 +0200

    Import fetch and copy paste Kerim's example of how to create multiple promises and waiting for all of their responses before proceeding

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 const serverSettings = require("./settings.json");
+// const fetch = (...args) =>
+//   import("node-fetch").then(({ default: fetch }) => fetch(...args));
 
 console.log(serverSettings);
+console.log(serverSettings.devices.length);
+
+async function main() {
+  // Use this call to fetch api on Kerim's end
+  // const promises = serverSettings.devices.map((device) =>
+  //   fetch(`http://${device}/cm?cmnd=STATUS%200`)
+  //     .then((res) => res.json())
+  //     .catch((err) => console.error(err))
+  // );
+  // const results = await Promise.all(promises);
+  // console.log(results.length);
+}
+
+main();
